e0cc721999fe162efd84e479c7cd879beaca9262
Giacomo Riboldi <giacomo.riboldi@mail.polimi.it>
2021-04-09 14:02:18 +0200

    perfezionamento codice blackbox greybox con dati filtrati correttamente tolto il - agli encoder del nominal_model

--- Model/blackbox_est.m
+++ Model/blackbox_est.m
@@ -1,108 +1,83 @@
 %% Blackbox estimation: 1dof
 
-data1 = load( 'voltage_step_1dof_-10V.mat', 'data');
-data2 = load( 'voltage_step_1dof_-8V.mat', 'data');
-data3 = load( 'voltage_step_1dof_-6V.mat', 'data');
-data4 = load( 'voltage_step_1dof_-4V.mat', 'data');
-data5 = load( 'voltage_step_1dof_-2V.mat', 'data');
-data6 = load( 'voltage_step_1dof_2V.mat', 'data');
-data7 = load( 'voltage_step_1dof_4V.mat', 'data');
-data8 = load( 'voltage_step_1dof_6V.mat', 'data');
-data9 = load( 'voltage_step_1dof_8V.mat', 'data');
-data10 = load( 'voltage_step_1dof_10V.mat', 'data');
+%% Dati con filtro a 25rad/s, con filtro spostato a 25Hz
 
-data1_filtered = load( 'voltage_step_1dof_-10V_filterok.mat', 'data');
-data2_filtered = load( 'voltage_step_1dof_-6V_filterok.mat', 'data');
-data3_filtered = load( 'voltage_step_1dof_2V_filterok.mat', 'data');
+titoli = { 'voltage_step_1dof_-10V.mat', 'voltage_step_1dof_-8V.mat', 'voltage_step_1dof_-6V.mat', ...
+    'voltage_step_1dof_-4V.mat', 'voltage_step_1dof_-2V.mat', 'voltage_step_1dof_2V.mat', ...
+    'voltage_step_1dof_4V.mat', 'voltage_step_1dof_6V.mat', 'voltage_step_1dof_8V.mat', ...
+    'voltage_step_1dof_10V.mat' };
 
-experiment1_filterok = iddata( -filter_transform( data1.data.time, data1.data.mass1_vel ), data1.data.voltage', 0.002 );
-experiment2_filterok = iddata( -filter_transform( data2.data.time, data2.data.mass1_vel ), data2.data.voltage', 0.002 );
-experiment3_filterok = iddata( -filter_transform( data3.data.time, data3.data.mass1_vel ), data3.data.voltage', 0.002 );
-experiment4_filterok = iddata( -filter_transform( data4.data.time, data4.data.mass1_vel ), data4.data.voltage', 0.002 );
-experiment5_filterok = iddata( -filter_transform( data5.data.time, data5.data.mass1_vel ), data5.data.voltage', 0.002 );
-experiment6_filterok = iddata( -filter_transform( data6.data.time, data6.data.mass1_vel ), data6.data.voltage', 0.002 );
-experiment7_filterok = iddata( -filter_transform( data7.data.time, data7.data.mass1_vel ), data7.data.voltage', 0.002 );
-experiment8_filterok = iddata( -filter_transform( data8.data.time, data8.data.mass1_vel ), data8.data.voltage', 0.002 );
-experiment9_filterok = iddata( -filter_transform( data9.data.time, data9.data.mass1_vel ), data9.data.voltage', 0.002 );
-experiment10_filterok = iddata( -filter_transform( data10.data.time, data10.data.mass1_vel ), data10.data.voltage', 0.002 );
+load( titoli{1}, 'data' );
+experiment = iddata( data.mass1_vel', data.voltage', 0.002 );
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    experiment2 = iddata( data.mass1_vel', data.voltage', 0.002 );
+    experiment = merge( experiment, experiment2 );
+end
 
-experiment1_filtered = iddata( -filter_transform( data1_filtered.data.time, data1_filtered.data.mass1_vel ), data1_filtered.data.voltage', 0.002 );
-experiment2_filtered = iddata( -filter_transform( data2_filtered.data.time, data2_filtered.data.mass1_vel ), data2_filtered.data.voltage', 0.002 );
-experiment3_filtered = iddata( -filter_transform( data3_filtered.data.time, data3_filtered.data.mass1_vel ), data3_filtered.data.voltage', 0.002 );
+blackbox_id_1dof = tfest( experiment, 5, 1 );
+blackbox_id_1dof.Notes = "Dati con filtro a 25rad/s, con filtro spostato a 25Hz";
+blackbox_id_1dof.Name = "blackbox id 1dof";
 
-experiment1 = iddata( -data1.data.mass1_vel', data1.data.voltage', 0.002 );
-experiment2 = iddata( -data2.data.mass1_vel', data2.data.voltage', 0.002 );
-experiment3 = iddata( -data3.data.mass1_vel', data3.data.voltage', 0.002 );
-experiment4 = iddata( -data4.data.mass1_vel', data4.data.voltage', 0.002 );
-experiment5 = iddata( -data5.data.mass1_vel', data5.data.voltage', 0.002 );
-experiment6 = iddata( -data6.data.mass1_vel', data6.data.voltage', 0.002 );
-experiment7 = iddata( -data7.data.mass1_vel', data7.data.voltage', 0.002 );
-experiment8 = iddata( -data8.data.mass1_vel', data8.data.voltage', 0.002 );
-experiment9 = iddata( -data9.data.mass1_vel', data9.data.voltage', 0.002 );
-experiment10 = iddata( -data10.data.mass1_vel', data10.data.voltage', 0.002 );
+figure
+bode( blackbox_id_1dof ); hold on; legend
 
-experiment_compare = iddata( -data_compare.mass1_vel(:,2), data_compare.voltage(:,2), 0.002 );
+%% Dati con filtro a 25Hz
 
-experiment = merge( experiment1, experiment2, experiment3, experiment4, experiment5, ...
-   experiment6, experiment7, experiment8, experiment9, experiment10);
+titoli = { 'voltage_step_1dof_-10V_filterok.mat', 'voltage_step_1dof_-6V_filterok.mat', 'voltage_step_1dof_2V_filterok.mat' };
 
-experiment_filterok = merge( experiment1_filterok, experiment2_filterok, experiment3_filterok, experiment4_filterok, ...
-    experiment5_filterok, experiment6_filterok, experiment7_filterok, experiment8_filterok, experiment9_filterok, ...
-    experiment10_filterok );
+load( titoli{1}, 'data' );
+experiment = iddata( data.mass1_vel', data.voltage', 0.002 );
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    experiment2 = iddata( data.mass1_vel', data.voltage', 0.002 );
+    experiment = merge( experiment, experiment2 );
+end
 
-experiment_filtered = merge( experiment1_filtered, experiment2_filtered, experiment3_filtered );
+blackbox_id_1dof_filterok = tfest( experiment, 5, 1 );
+blackbox_id_1dof_filterok.Notes = "Dati con filtro a 25Hz";
+blackbox_id_1dof_filterok.Name = "blackbox id 1dof filterOk";
 
-%experiment = merge( experiment1, experiment3, experiment6 );
-%data_to_fit = iddata( data1.mass1_vel', data1.voltage', 0.002 );
-
-% data_to_fit = EXPERIMENT_HANDLER( TIME INZIIO, % NUMERO DI ESP/TIME FINE,
-% INOUT, OUTPUT); 
-
-blackbox_id_1dof = tfest( experiment, 5, 1 )
-blackbox_id_1dof_filterok = tfest( experiment_filterok, 5, 1 )
-blackbox_id_1dof_filtered = tfest( experiment_filtered, 5, 1 )
-
-blackbox_compare = tfest( experiment_compare, 5, 1 )
-
-% bode( blackbox_id_1dof );
-% grid on;
-% damp(blackbox_id_1dof)
+bode( blackbox_id_1dof_filterok );
 
 %% Blackbox estimation: 2dof
 
-data1 = load( 'voltage_step_2dof_-10V.mat', 'data');
-data2 = load( 'voltage_step_2dof_-8V.mat', 'data');
-data3 = load( 'voltage_step_2dof_-6V.mat', 'data');
-data4 = load( 'voltage_step_2dof_-4V.mat', 'data');
-data5 = load( 'voltage_step_2dof_-2V.mat', 'data');
-data6 = load( 'voltage_step_2dof_2V.mat', 'data');
-data7 = load( 'voltage_step_2dof_4V.mat', 'data');
-data8 = load( 'voltage_step_2dof_6V.mat', 'data');
-data9 = load( 'voltage_step_2dof_8V.mat', 'data');
-data10 = load( 'voltage_step_2dof_10V.mat', 'data');
+%% Dati con filtro a 25rad/s, con filtro spostato a 25Hz
 
-experiment1 = iddata( -data1.data.mass2_vel', data1.data.voltage', 0.002 );
-experiment2 = iddata( -data2.data.mass2_vel', data2.data.voltage', 0.002 );
-experiment3 = iddata( -data3.data.mass2_vel', data3.data.voltage', 0.002 );
-experiment4 = iddata( -data4.data.mass2_vel', data4.data.voltage', 0.002 );
-experiment5 = iddata( -data5.data.mass2_vel', data5.data.voltage', 0.002 );
-experiment6 = iddata( -data6.data.mass2_vel', data6.data.voltage', 0.002 );
-experiment7 = iddata( -data7.data.mass2_vel', data7.data.voltage', 0.002 );
-experiment8 = iddata( -data8.data.mass2_vel', data8.data.voltage', 0.002 );
-experiment9 = iddata( -data9.data.mass2_vel', data9.data.voltage', 0.002 );
-experiment10 = iddata( -data10.data.mass2_vel', data10.data.voltage', 0.002 );
+titoli = { 'voltage_step_2dof_-10V.mat', 'voltage_step_2dof_-8V.mat', 'voltage_step_2dof_-6V.mat', ...
+    'voltage_step_2dof_-4V.mat', 'voltage_step_2dof_-2V.mat', 'voltage_step_2dof_2V.mat', ...
+    'voltage_step_2dof_4V.mat', 'voltage_step_2dof_6V.mat', 'voltage_step_2dof_8V.mat', ...
+    'voltage_step_2dof_10V.mat' };
 
-experiment = merge( experiment1, experiment2, experiment3, experiment4, experiment5, ...
-   experiment6, experiment7, experiment8, experiment9, experiment10);
-%experiment = merge( experiment1, experiment3, experiment6 );
-%data_to_fit = iddata( data1.mass1_vel', data1.voltage', 0.002 );
+load( titoli{1}, 'data' );
+experiment = iddata( data.mass2_vel', data.voltage', 0.002 );
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    experiment2 = iddata( data.mass2_vel', data.voltage', 0.002 );
+    experiment = merge( experiment, experiment2 );
+end
 
-% data_to_fit = EXPERIMENT_HANDLER( TIME INZIIO, % NUMERO DI ESP/TIME FINE,
-% INOUT, OUTPUT); 
+blackbox_id_2dof = tfest( experiment, 7, 4 );
+blackbox_id_2dof.Notes = "Dati con filtro a 25rad/s, con filtro spostato a 25Hz";
+blackbox_id_2dof.Name = "blackbox id 2dof";
 
-blackbox_id_2dof = tfest( experiment, 7 )
+figure
+bode( blackbox_id_2dof ); hold on; legend
 
-% bode( blackbox_id_1dof );
-% grid on;
-% damp(blackbox_id_1dof)
+%% Dati con filtro a 25Hz
 
+titoli = { 'voltage_step_2dof_-4V_filterok.mat', 'voltage_step_2dof_2V_filterok.mat' };
+
+load( titoli{1}, 'data' );
+experiment = iddata( data.mass2_vel', data.voltage', 0.002 );
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    experiment2 = iddata( data.mass2_vel', data.voltage', 0.002 );
+    experiment = merge( experiment, experiment2 );
+end
+
+blackbox_id_2dof_filterok = tfest( experiment, 7, 4 );
+blackbox_id_2dof_filterok.Notes = "Dati con filtro a 25Hz";
+blackbox_id_2dof_filterok.Name = "blackbox id 2dof filterOk";
+
+bode( blackbox_id_2dof_filterok );
--- Model/greybox_est.m
+++ Model/greybox_est.m
@@ -1,70 +1,95 @@
 %% Greybox estimation: 1-dof
 
-data1 = load( 'voltage_step_1dof_-10V_filterok.mat', 'data');
-% data2 = load( 'voltage_step_1dof_-8V.mat', 'data');
-data3 = load( 'voltage_step_1dof_-6V_filterok.mat', 'data');
-% data4 = load( 'voltage_step_1dof_-4V.mat', 'data');
-% data5 = load( 'voltage_step_1dof_-2V.mat', 'data');
-data6 = load( 'voltage_step_1dof_2V_filterok.mat', 'data');
-% data7 = load( 'voltage_step_1dof_4V.mat', 'data');
-% data8 = load( 'voltage_step_1dof_6V.mat', 'data');
-% data9 = load( 'voltage_step_1dof_8V.mat', 'data');
-% data10 = load( 'voltage_step_1dof_10V.mat', 'data');
-% dataX = load( 'sinesweep_1V_0_10Hz_ave5V.mat', 'data' );
-% figure
-% plot(dataX.data.time, dataX.data.mass1_vel); hold on
-% w_filter = 2*pi*25;
-% dataX.data.mass1_vel = lsim( tf(w_filter,[1 w_filter]), dataX.data.mass1_vel, (0:length(dataX.data.mass1_vel)-1)*0.002 )';
-% plot(dataX.data.time, dataX.data.mass1_vel)
+%% Dati con filtro a 25rad/s, con filtro spostato a 25Hz
 
-% experiment1 = iddata( data1.data.mass1_vel', data1.data.voltage', 0.002 );
-% experiment2 = iddata( data2.data.mass1_vel', data2.data.voltage', 0.002 );
-% experiment3 = iddata( data3.data.mass1_vel', data3.data.voltage', 0.002 );
-% experiment4 = iddata( data4.data.mass1_vel', data4.data.voltage', 0.002 );
-% experiment5 = iddata( data5.data.mass1_vel', data5.data.voltage', 0.002 );
-% experiment6 = iddata( data6.data.mass1_vel', data6.data.voltage', 0.002 );
-% experiment7 = iddata( data7.data.mass1_vel', data7.data.voltage', 0.002 );
-% experiment8 = iddata( data8.data.mass1_vel', data8.data.voltage', 0.002 );
-% experiment9 = iddata( data9.data.mass1_vel', data9.data.voltage', 0.002 );
-% experiment10 = iddata( data10.data.mass1_vel', data10.data.voltage', 0.002 );
+titoli = { 'voltage_step_1dof_-10V.mat', 'voltage_step_1dof_-8V.mat', 'voltage_step_1dof_-6V.mat', ...
+    'voltage_step_1dof_-4V.mat', 'voltage_step_1dof_-2V.mat', 'voltage_step_1dof_2V.mat', ...
+    'voltage_step_1dof_4V.mat', 'voltage_step_1dof_6V.mat', 'voltage_step_1dof_8V.mat', ...
+    'voltage_step_1dof_10V.mat' };
 
-% experiment = { experiment1, experiment3, experiment6 };
-
-% input = data.voltage';
-% outputs = data.mass1_vel';
-
-inputs = {data1.data.voltage', data3.data.voltage', data6.data.voltage'};
-outputs = {-data1.data.mass1_vel', -data3.data.mass1_vel', -data6.data.mass1_vel'};
+load( titoli{1}, 'data' );
+inputs{1,1} = data.voltage';
+outputs{1,1} = data.mass1_vel';
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    inputs{1,idx} = data.voltage';
+    outputs{1,idx} = data.mass1_vel';
+end
 
 data_to_est = iddata( outputs, inputs, 0.002 );
 
-opt = greyestOptions('EnforceStability',true, ...
-    'WeightingFilter', [0.01,70] );
+opt = greyestOptions('EnforceStability',true, 'WeightingFilter', [0.01,70] );
 opt.SearchOptions.MaxIterations = 40;
 greybox_id_1dof = greyest( data_to_est, nominal_sys_1dof, opt );
 
 figure
-bode( greybox_id_1dof, {0.1,100} ); hold on
-bode( G_opt )
-bode( nominal_sys_1dof )
+bode( greybox_id_1dof, {0.1,100} ); hold on; legend
 
-%% Greybox estimation: 2-dof
+%% Dati con filtro a 25Hz
 
-data1 = load( 'voltage_step_2dof_-10V_filterok.mat', 'data');
-data2 = load( 'voltage_step_2dof_-4V_filterok.mat', 'data');
-data3 = load( 'voltage_step_2dof_2V_filterok.mat', 'data');
+titoli = { 'voltage_step_1dof_-10V_filterok.mat', 'voltage_step_1dof_-6V_filterok.mat', 'voltage_step_1dof_2V_filterok.mat' };
 
-inputs = {data1.data.voltage', data2.data.voltage', data3.data.voltage'};
-outputs = {-data1.data.mass1_vel', -data2.data.mass1_vel', -data3.data.mass1_vel'};
+load( titoli{1}, 'data' );
+inputs{1,1} = data.voltage';
+outputs{1,1} = data.mass1_vel';
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    inputs{1,idx} = data.voltage';
+    outputs{1,idx} = data.mass1_vel';
+end
 
 data_to_est = iddata( outputs, inputs, 0.002 );
 
-opt = greyestOptions('EnforceStability',true, ...
-    'WeightingFilter', [0.01,70] );
+opt = greyestOptions('EnforceStability',true, 'WeightingFilter', [0.01,70] );
+opt.SearchOptions.MaxIterations = 40;
+greybox_id_1dof_filterok = greyest( data_to_est, nominal_sys_1dof, opt );
+
+bode( greybox_id_1dof_filterok, {0.1,100} )
+
+%% Greybox estimation: 2-dof
+
+%% Dati con filtro a 25rad/s, con filtro spostato a 25Hz
+
+titoli = { 'voltage_step_2dof_-10V.mat', 'voltage_step_2dof_-8V.mat', 'voltage_step_2dof_-6V.mat', ...
+    'voltage_step_2dof_-4V.mat', 'voltage_step_2dof_-2V.mat', 'voltage_step_2dof_2V.mat', ...
+    'voltage_step_2dof_4V.mat', 'voltage_step_2dof_6V.mat', 'voltage_step_2dof_8V.mat', ...
+    'voltage_step_2dof_10V.mat' };
+
+load( titoli{1}, 'data' );
+inputs{1,1} = data.voltage';
+outputs{1,1} = data.mass2_vel';
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    inputs{1,idx} = data.voltage';
+    outputs{1,idx} = data.mass2_vel';
+end
+
+data_to_est = iddata( outputs, inputs, 0.002 );
+
+opt = greyestOptions('EnforceStability',true, 'WeightingFilter', [0.01,70] );
 opt.SearchOptions.MaxIterations = 60;
 greybox_id_2dof = greyest( data_to_est, nominal_sys_2dof, opt );
 
 figure
-bode( greybox_id_2dof, {0.1,100} ); hold on
-bode( G_opt )
-bode( nominal_sys_2dof )
+bode( greybox_id_2dof, {0.1,100} ); hold on; legend
+
+%% Dati con filtro a 25Hz
+
+titoli = { 'voltage_step_2dof_-4V_filterok.mat', 'voltage_step_2dof_2V_filterok.mat' };
+
+load( titoli{1}, 'data' );
+inputs{1,1} = data.voltage';
+outputs{1,1} = data.mass2_vel';
+for idx = 2:length( titoli )
+    load( titoli{idx}, 'data' );
+    inputs{1,idx} = data.voltage';
+    outputs{1,idx} = data.mass2_vel';
+end
+
+data_to_est = iddata( outputs, inputs, 0.002 );
+
+opt = greyestOptions('EnforceStability',true, 'WeightingFilter', [0.01,70] );
+opt.SearchOptions.MaxIterations = 60;
+greybox_id_2dof_filterok = greyest( data_to_est, nominal_sys_2dof, opt );
+
+bode( greybox_id_2dof_filterok, {0.1,100} )
