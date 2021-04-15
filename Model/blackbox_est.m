398b386fb48ca6ad57ad2c074d2be79c3755698a
Dennis <dennis.zanutto@mail.polimi.it>
2021-04-15 18:51:49 +0200

    BB e introduction tex

--- Model/blackbox_est.m
+++ Model/blackbox_est.m
@@ -15,30 +15,12 @@ for idx = 2:length( titoli )
     experiment = merge( experiment, experiment2 );
 end
 
-blackbox_id_1dof = tfest( experiment, 5, 1 );
+blackbox_id_1dof = tfest( experiment, 4, 0 );
 blackbox_id_1dof.Notes = "Dati con filtro a 25 Hz";
 blackbox_id_1dof.Name = "blackbox id 1dof";
 
 figure
 bode( blackbox_id_1dof, {1,150} ); hold on; legend
-
-%% Dati con filtro a 25 Hz (dati filterok)
-
-% titoli = { 'voltage_step_1dof_-10V_filterok.mat', 'voltage_step_1dof_-6V_filterok.mat', 'voltage_step_1dof_2V_filterok.mat' };
-% 
-% load( titoli{1}, 'data' );
-% experiment = iddata( data.mass1_vel', data.voltage', 0.002 );
-% for idx = 2:length( titoli )
-%     load( titoli{idx}, 'data' );
-%     experiment2 = iddata( data.mass1_vel', data.voltage', 0.002 );
-%     experiment = merge( experiment, experiment2 );
-% end
-% 
-% blackbox_id_1dof_filterok = tfest( experiment, 5, 1 );
-% blackbox_id_1dof_filterok.Notes = "Dati con filtro a 25Hz";
-% blackbox_id_1dof_filterok.Name = "blackbox id 1dof filterOk";
-% 
-% bode( blackbox_id_1dof_filterok );
 
 %% Blackbox estimation: 2dof
 
@@ -57,27 +39,10 @@ for idx = 2:length( titoli )
     experiment = merge( experiment, experiment2 );
 end
 
-blackbox_id_2dof = tfest( experiment, 7, 4 );
+blackbox_id_2dof = tfest( experiment, 6, 0 );
 blackbox_id_2dof.Notes = "Dati con filtro a 25 Hz";
 blackbox_id_2dof.Name = "blackbox id 2dof";
 
 figure
 bode( blackbox_id_2dof, {1,150} ); hold on; legend
 
-%% Dati con filtro a 25 Hz (dati filterok)
-
-% titoli = { 'voltage_step_2dof_-4V_filterok.mat', 'voltage_step_2dof_2V_filterok.mat' };
-% 
-% load( titoli{1}, 'data' );
-% experiment = iddata( data.mass2_vel', data.voltage', 0.002 );
-% for idx = 2:length( titoli )
-%     load( titoli{idx}, 'data' );
-%     experiment2 = iddata( data.mass2_vel', data.voltage', 0.002 );
-%     experiment = merge( experiment, experiment2 );
-% end
-% 
-% blackbox_id_2dof_filterok = tfest( experiment, 7, 4 );
-% blackbox_id_2dof_filterok.Notes = "Dati con filtro a 25Hz";
-% blackbox_id_2dof_filterok.Name = "blackbox id 2dof filterOk";
-% 
-% bode( blackbox_id_2dof_filterok );
