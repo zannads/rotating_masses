9046d7a173f19aaf5db305dabc4d1e54c34cae0a
Dennis <dennis.zanutto@mail.polimi.it>
2021-06-07 14:05:26 +0200

    plot_tf and some new images

--- Model/Post_processing/plot_tf.m
+++ Model/Post_processing/plot_tf.m
@@ -1,33 +1,33 @@
-Gn_v_w1_1dof = minreal(nominal_sys_1dof.C(end,:)/(s*eye(4)-nominal_sys_1dof.A)*nominal_sys_1dof.B)
-Gn_v_w1_2dof = minreal(nominal_sys_2dof.C(2,:)/(s*eye(6)-nominal_sys_2dof.A)*nominal_sys_2dof.B)
-Gn_v_w2_2dof = minreal(nominal_sys_2dof.C(end,:)/(s*eye(6)-nominal_sys_2dof.A)*nominal_sys_2dof.B)
+Gn_v_w1_1dof = minreal([0,0,0,-1]/(s*eye(4)-nominal_sys_1dof.A)*nominal_sys_1dof.B)
+Gn_v_w1_2dof = minreal([0,0,0,-1,0,0]/(s*eye(6)-nominal_sys_2dof.A)*nominal_sys_2dof.B)
+Gn_v_w2_2dof = minreal([0,0,0,0,0,-1]/(s*eye(6)-nominal_sys_2dof.A)*nominal_sys_2dof.B)
 
-Gg_v_w1_1dof = minreal(greybox_id_1dof.C(end,:)/(s*eye(4)-greybox_id_1dof.A)*greybox_id_1dof.B)
-Gg_v_w1_2dof = minreal(greybox_id_2dof.C(2,:)/(s*eye(6)-greybox_id_2dof.A)*greybox_id_2dof.B)
-Gg_v_w2_2dof = minreal(greybox_id_2dof.C(end,:)/(s*eye(6)-greybox_id_2dof.A)*greybox_id_2dof.B)
+Gg_v_w1_1dof = minreal([0,0,0,-1]/(s*eye(4)-greybox_id_1dof.A)*greybox_id_1dof.B)
+Gg_v_w1_2dof = minreal([0,0,0,-1,0,0]/(s*eye(6)-greybox_id_2dof.A)*greybox_id_2dof.B)
+Gg_v_w2_2dof = minreal([0,0,0,0,0,-1]/(s*eye(6)-greybox_id_2dof.A)*greybox_id_2dof.B)
 
 figure;
 hold on;
-bode( Gn_v_w1_1dof ); 
-bode( Gg_v_w1_1dof );
-bode( blackbox_id_1dof ); 
+bode( Gn_v_w1_1dof, 'm'); 
+bode( Gg_v_w1_1dof, 'b' );
+bode( blackbox_id_1dof*tf([1/140 1], 1 ), 'k' ); 
 legend( '$G^{nom}$ ', '$$G^{grey}$$', '$$G^{black}$$', 'Interpreter', 'latex' );
 grid on;
 title( '1-dof $G_{v,\dot \theta _1}$ comparison', 'Interpreter', 'latex');
 
 figure;
 hold on;
-bode( Gn_v_w1_2dof ); 
-bode( Gg_v_w1_2dof );
+bode( Gn_v_w1_2dof, 'm' ); 
+bode( Gg_v_w1_2dof, 'b' );
 legend( '$G^{nom}$ ', '$$G^{grey}$$', 'Interpreter', 'latex' );
 grid on;
 title( '2-dof $G_{v,\dot \theta _1}$ comparison', 'Interpreter', 'latex');
 
 figure;
 hold on;
-bode( Gn_v_w2_2dof ); 
-bode( Gg_v_w2_2dof );
-bode( blackbox_id_2dof ); 
+bode( Gn_v_w2_2dof, 'm' ); 
+bode( Gg_v_w2_2dof, 'b' );
+bode( blackbox_id_2dof*tf([1/91.8 1], 1 ), 'k' ); 
 legend( '$G^{nom}$ ',  '$$G^{grey}$$','$$G^{black}$$', 'Interpreter', 'latex' );
 grid on;
 title( '2-dof $G_{v,\dot \theta _2}$ comparison', 'Interpreter', 'latex');
