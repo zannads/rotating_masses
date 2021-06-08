a2a95403f499c5a37a08de65dc36a1c58b0acfab
Giacomo Riboldi <giacomo.riboldi@mail.polimi.it>
2021-06-08 14:01:27 +0200

    Correzioni ortografiche e di impaginazione. Ridimensionate/sostituite alcune immagini. Chapter 6 LQ: aggiunti i K dei controllori

--- Model/Post_processing/blackbox_est.m
+++ Model/Post_processing/blackbox_est.m
@@ -15,12 +15,43 @@ for idx = 2:length( titoli )
     experiment = merge( experiment, experiment2 );
 end
 
+blackbox_id_1dof_withZeros = tfest( experiment, 4, 2 );
+blackbox_id_1dof_withZeros.Notes = "Dati con filtro a 25 Hz";
+blackbox_id_1dof_withZeros.Name = "blackbox id 1dof with zeros";
+
 blackbox_id_1dof = tfest( experiment, 4, 0 );
 blackbox_id_1dof.Notes = "Dati con filtro a 25 Hz";
 blackbox_id_1dof.Name = "blackbox id 1dof";
 
+[magn1,phas1,w1] = bode(blackbox_id_1dof_withZeros);
+[magn2,phas2,w2] = bode(blackbox_id_1dof);
+
 figure
-bode( blackbox_id_1dof, {1,150} ); hold on; legend
+subplot(2,1,1)
+plot(w1,20*log10(magn1(:)))
+set(gca, 'XScale', 'log')
+ylabel( 'Magnitude (dB)' )
+grid on; hold on
+plot(w2,20*log10(magn2(:)))
+legend( '$G_{V,\dot{\theta}_1}$ with zeros', '$G_{V,\dot{\theta}_1}$ without zeros', ...
+    'Interpreter', 'Latex' )
+title( '2-dof $G_{V,\dot{\theta}_1}$ black-boxes', 'Interpreter', 'Latex' )
+xlim( [10, 200] )
+
+subplot(2,1,2)
+plot(w1,phas1(:))
+set(gca, 'XScale', 'log')
+xlabel( 'Frequency (rad/s)' )
+ylabel( 'Phase (deg)' )
+grid on; hold on
+plot(w2,phas2(:))
+xlim( [10, 200] )
+ylim( [-180,180] )
+
+% bode( blackbox_id_1dof_withZeros, {10,200} );
+% grid on; hold on; legend
+% bode( blackbox_id_1dof, {10,200} );
+% title( 'Bode diagram: $G_{v,\dot{\theta}_1}$', 'Interpreter', 'Latex' )
 
 %% Blackbox estimation: 2dof
 
@@ -39,10 +70,39 @@ for idx = 2:length( titoli )
     experiment = merge( experiment, experiment2 );
 end
 
+blackbox_id_2dof_withZeros = tfest( experiment, 6, 2 );
+blackbox_id_2dof_withZeros.Notes = "Dati con filtro a 25 Hz";
+blackbox_id_2dof_withZeros.Name = "blackbox id 2dof with zeros";
+
 blackbox_id_2dof = tfest( experiment, 6, 0 );
 blackbox_id_2dof.Notes = "Dati con filtro a 25 Hz";
 blackbox_id_2dof.Name = "blackbox id 2dof";
 
+[magn1,phas1,w1] = bode(blackbox_id_2dof_withZeros);
+[magn2,phas2,w2] = bode(blackbox_id_2dof);
+
 figure
-bode( blackbox_id_2dof, {1,150} ); hold on; legend
+subplot(2,1,1)
+plot(w1,20*log10(magn1(:)))
+set(gca, 'XScale', 'log')
+ylabel( 'Magnitude (dB)' )
+grid on; hold on
+plot(w2,20*log10(magn2(:)))
+legend( '$G_{V,\dot{\theta}_2}$ with zeros', '$G_{V,\dot{\theta}_2}$ without zeros', ...
+    'Interpreter', 'Latex' )
+title( '2-dof $G_{V,\dot{\theta}_1}$ black-boxes', 'Interpreter', 'Latex' )
+xlim( [10, 200] )
+
+subplot(2,1,2)
+plot(w1,phas1(:))
+set(gca, 'XScale', 'log')
+xlabel( 'Frequency (rad/s)' )
+ylabel( 'Phase (deg)' )
+grid on; hold on
+plot(w2,phas2(:))
+xlim( [10, 200] )
+ylim( [-360,180] )
+
+% figure
+% bode( blackbox_id_2dof, {1,150} ); hold on; legend
 
